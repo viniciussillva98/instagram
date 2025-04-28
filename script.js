c95110abe67097cc226b88fa3e2ec060b593e6f1
viniciussillva98 <viniciussaldanha1998@gmail.com>
2025-04-28 13:16:34 -0300

    script.js-no-projeto

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+let imagem = document.querySelector(".tocaimagem")
+
+function functiontrocaimagem() {
+
+    /*se*/
+    if (imagem.style.opacity == 0) { imagem.style.opacity = 1 }
+
+    /*se não*/
+    else { imagem.style.opacity = 0 }
+
+
+}
+
+setInterval(functiontrocaimagem, 4000)
